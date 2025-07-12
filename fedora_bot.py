887140e21e495e45c2464a530b9f05b92d43a774
Tomáš Hozza <thozza@redhat.com>
2025-07-12 14:08:31 +0200

    fedora-bot: handle 'error' status in PR test results
    Extend the list of gracefully handled test statuses in a Pagure PR by
    'error'. Previously, the code would raise an exception, which would
    result in the following component PRs not being processed.
    
    In addition, unify all messages printed by the bot to contain consistent
    information about the PR that is being handled and also list the test
    results in unexpected situations.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- fedora_bot.py
+++ fedora_bot.py
@@ -139,16 +139,18 @@ def check_pull_request_flags(http, component, pr_id, num_tests):
         test_results.append(flag['status'])
 
     if len(test_results) != num_tests: # check if the expected number of tests passed
-        msg_info(f"Only {len(test_results)}/{num_tests} tests have run, let's try again later.")
+        msg_info(f"Pull request '{pr_id}' has {len(test_results)} test results, but expected {num_tests}.")
     elif all(r == 'success' for r in test_results):
-        msg_ok(f"All {len(test_results)} tests passed so the pull-request can be merged.")
+        msg_ok(f"Pull request '{pr_id}' has passed all tests and can be auto-merged.")
         success = True
     elif 'failure' in test_results:
-        msg_info(f"Pull request '{pr_id}' has {len(test_results)}/{num_tests} failed tests and therefore cannot be auto-merged.")
+        msg_info(f"Pull request '{pr_id}' has failed tests and therefore cannot be auto-merged.")
     elif 'pending' in test_results:
-        msg_info("Some tests are still running, let's try again later")
+        msg_info(f"Pull request '{pr_id}' has tests that are still pending and therefore cannot be auto-merged.")
+    elif 'error' in test_results:
+        msg_info(f"Pull request '{pr_id}' has tests with errors and therefore cannot be auto-merged.")
     else:
-        msg_error("Something is wrong - maybe the amount of tests have changed?")
+        msg_error(f"Pull request '{pr_id}' has unexpected test results: {res['flags']}")
 
     return success
 
